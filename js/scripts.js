a427b4ff65afe0fbd61283abede9c49251e64d87
kyle dean reinford <kyledeanreinford@gmail.com>
2018-02-13 12:52:28 -0600

    Added slideshow transitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,35 @@
 
 // Main background images for slideshow
-const imageArray = ["./images/slideshow_images/optimized/SINA_slideshow-01.jpg","./images/slideshow_images/optimized/SINA_slideshow-02.jpg","./images/slideshow_images/optimized/SINA_slideshow-03.jpg","./images/slideshow_images/optimized/SINA_slideshow-04.jpg","./images/slideshow_images/optimized/SINA_slideshow-05.jpg","./images/slideshow_images/optimized/SINA_slideshow-06.jpg" ]
+const imgArray = ["./images/slideshow_images/optimized/SINA_slideshow-01.jpg","./images/slideshow_images/optimized/SINA_slideshow-02.jpg","./images/slideshow_images/optimized/SINA_slideshow-03.jpg","./images/slideshow_images/optimized/SINA_slideshow-04.jpg","./images/slideshow_images/optimized/SINA_slideshow-05.jpg","./images/slideshow_images/optimized/SINA_slideshow-06.jpg" ]
 
 // Slideshow function
+// function carousel() {
+//     let index = 0;
+//     setInterval(function() {
+//     let headlineImage = document.getElementById('headlineImage');
+//     headlineImage.src = imageArray[index];
+//     index += 1;
+//     if (index === imageArray.length) {
+//       index = 0;
+//     }
+//   }, 5000);
+// }
 
+var index = 0;
+const imgDuration = 7000;
 
 function carousel() {
-    let index = 0;
-    setInterval(function() {
-    let headlineImage = document.getElementById('headlineImage');
-    headlineImage.className += "fadeOut";
-    headlineImage.src = imageArray[index];
-    headlineImage.className = "";
+    document.getElementById('headlineImage').className += "fadeOut";
+    setTimeout(function() {
+        document.getElementById('headlineImage').src = imgArray[index];
+        document.getElementById('headlineImage').className = "";
+    }, 1000);
     index += 1;
-    if (index === imageArray.length) {
+    if (index == imgArray.length) {
       index = 0;
     }
-}, 5000);
+    setTimeout(carousel, imgDuration);
 }
-
 
 document.onload = carousel();
 
